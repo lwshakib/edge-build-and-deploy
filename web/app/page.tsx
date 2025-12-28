39809ad7db8a094b7b865e1234d92fd0dd789ab2
lwshakib <leadwithshakib@gmail.com>
2025-12-28 19:03:06 +0600

    feat(home): add hero, pricing, and footer sections to homepage
    - Import HeroSection, Pricing, and FooterSection components
    - Render HeroSection at the top of the homepage
    - Add Pricing component below the hero section
    - Include FooterSection at the bottom of the homepage

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,14 @@
+import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+import Pricing from "@/components/pricing";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    
+    <HeroSection />
+    <Pricing/>
+    <FooterSection />
    </div>
   );
 }
